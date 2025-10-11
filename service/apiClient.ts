a36cffa6260d0d2fb16249e549c7423eab2eb6c9
Adeleye-Adeola <mightydeveloper500@gmail.com>
2025-10-11 04:19:27 +0100

    changed baseUrl

--- service/apiClient.ts
+++ service/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const DataApiClient = axios.create({
-    baseURL: "https://sdc-backend-drbw.onrender.com",
+    baseURL: "hhttps://sdc-backend-7173.onrender.com",
     // baseURL: "http://127.0.0.1:7007",
     withCredentials: true,
 });
